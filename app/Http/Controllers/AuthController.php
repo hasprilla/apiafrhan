1677ca882d0cfca0968c26f5d9b2d68f28f97d46
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-05-05 18:39:03 -0500

    Logout solo res

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,21 +46,10 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        // $request->user()->currentAccessToken()->delete();
         return response()->json([
             'res' => true,
             'message' => 'Token eliminado correctamente'
         ], 200);
     }
-
-
-    // public function logout(Request $request)
-    // {
-    //     $request->user()->currentAccessToken()->delete();
-    //     return response()->json([
-    //         'res' => true,
-    //         'message' => 'Token eliminado correctamente',
-    //     ], 200);
-
-    // }
 }
