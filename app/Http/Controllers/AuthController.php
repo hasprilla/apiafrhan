54bdb661ae42c52e9003c0ba6c0d1e6401dc91cb
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-05-05 16:35:06 -0500

    Change function register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,114 +2,123 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\RegisterRequest;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    // /**
+    //  * Create a new AuthController instance.
+    //  *
+    //  * @return void
+    //  */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    // }
+
+    // /**
+    //  * Get a JWT via given credentials.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function login()
+    // {
+    //     $credentials = request(['email', 'password']);
+    //     if (!$token = auth()->attempt($credentials)) {
+    //         return response()->json(['error' => 'Unauthorized'], 401);
+    //     }
+
+
+    //     return $this->respondWithToken($token);
+    // }
+
+    // /**
+    //  * Get the authenticated User.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function me()
+    // {
+    //     return response()->json(auth()->user());
+    // }
+
+    // /**
+    //  * Log the user out (Invalidate the token).
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function logout()
+    // {
+    //     auth()->logout();
+
+    //     return response()->json(['message' => 'Successfully logged out']);
+    // }
+
+    // /**
+    //  * Refresh a token.
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // public function refresh()
+    // {
+    //     return $this->respondWithToken(auth()->refresh());
+    // }
+
+    // /**
+    //  * Get the token array structure.
+    //  *
+    //  * @param  string $token
+    //  *
+    //  * @return \Illuminate\Http\JsonResponse
+    //  */
+    // protected function respondWithToken($token)
+    // {
+    //     auth()->factory()->getTTL() * 60;
+
+    //     return response()->json([
+    //         'data' => [
+    //             'user' => with('roles') . auth()->user(),
+    //             'token' => $token,
+    //         ],
+    //     ]);
+    // }
+
+    public function register(RegisterRequest $request)
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    }
-
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login()
-    {
-        $credentials = request(['email', 'password']);
-        if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-
-
-        return $this->respondWithToken($token);
-    }
-
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
-    {
-        return response()->json(auth()->user());
-    }
-
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh()
-    {
-        return $this->respondWithToken(auth()->refresh());
-    }
-
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
-        auth()->factory()->getTTL() * 60;
-
-        return response()->json([
-            'data' => [
-                'user' => with('roles') . auth()->user(),
-                'token' => $token,
-            ],
-        ]);
-    }
-
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|string|email|max:100|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
-        }
-
-        $user = User::create(array_merge(
-            $validator->validate(),
-            ['password' => bcrypt($request->password)]
-        ));
-
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
         $user->roles()->attach($request->roles);
+
+
+        // $validator = Validator::make($request->all(), [
+        //     'name' => 'required',
+        //     'email' => 'required|string|email|max:100|unique:users',
+        //     'password' => 'required|string|min:6',
+        // ]);
+
+        // if ($validator->fails()) {
+        //     return response()->json($validator->errors()->toJson(), 400);
+        // }
+
+        // $user = User::create(array_merge(
+        //     $validator->validate(),
+        //     ['password' => bcrypt($request->password)]
+        // ));
+
+        // $user->roles()->attach($request->roles);
 
         return response()->json([
             'message' => '¡Usuario registrado exitosamente!',
             'user' => $user
-        ], 201);
+        ], 200);
     }
 }
