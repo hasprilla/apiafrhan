e4513daaff384138a09a2f95a15e56e992e62aaf
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-05-05 09:39:54 -0500

    ADD invalidJson and render

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +28,27 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'res' => __('Loca datos proporcionados no son validos.'),
+            'errores' => $exception->errors(),
+
+        ], $exception->status);
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'res' => false,
+                'error' => 'Error modelo no encontrado'
+
+            ], 400);
+        }
+
+        return parent::render($request, $exception);
     }
 }
