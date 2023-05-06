452d3f94517976bc7bb9472c9e4cdbbad9645e94
Harvey Asprilla <kikiyam_8@hotmail.com>
2023-05-05 19:12:25 -0500

    ADD RouteNotFoundException

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,9 +45,15 @@ class Handler extends ExceptionHandler
         if ($exception instanceof ModelNotFoundException) {
             return response()->json([
                 'res' => false,
-                'error' => 'Error modelo no encontrado'
-
+                'error' => 'Recurso no encontrado'
             ], 400);
+        }
+
+        if ($exception instanceof RouteNotFoundException) {
+            return response()->json([
+                'res' => false,
+                'error' => 'No tiene permiso para acceder a esta ruta'
+            ], 401);
         }
 
         return parent::render($request, $exception);
